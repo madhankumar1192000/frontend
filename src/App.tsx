c763f18a43bc06248170e7fc1a033e33bc938fd1
madhankumar1192000 <madhankumar1192000@gmail.com>
2025-08-28 17:41:14 +0530

    added contact to header

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Header from "./pages/header/Header";
 import Home from "./pages/home/Home";
 import About from "./pages/about/About";
 import Footer from "./pages/footer/Footer";
+import Contact from "./pages/contact/Contact";
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+
+        
       </Routes>
       <Footer />
     </>
